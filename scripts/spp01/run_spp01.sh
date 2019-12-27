af0344d82ba9b798126b4b3bfd18e06d2b793a34
darraghdog <darragh.hanley@gmail.com>
2019-12-27 14:22:53 -0600

    SPPNet

--- scripts/spp01/run_spp01.sh
+++ scripts/spp01/run_spp01.sh
@@ -1,12 +1,11 @@
 N_GPU=1
-WDIR='saver06'
+WDIR='spp01'
 FOLD=0
 SIZE='224'
 
-for FOLD in 0 1 2 3 4
-do
-    bsub  -q low2 -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:dfake \
-            -m dbslp1897 -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/dfake/util/$WDIR  && python3 vid_saver.py  \
-            --fold $FOLD  --rootpath /share/dhanley2/dfake/ --vidpath data/mount/train --metafile trainmeta.csv.gz  \
-            --imgpath data/mount/npimg --size $SIZE --startsize 640 --wtspath weights --fps 8"
-done
+bsub  -q low2 -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:dfake \
+            -m dbslp1897 -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/dfake/scripts/$WDIR  && python3 spp01.py  \
+            --fold $FOLD  --rootpath /share/dhanley2/dfake/ --metafile trainmeta.csv.gz  \
+            --imgpath data/mount/npimg --size $SIZE --batchsize 32 --epochs 5 --lr 0.0001"
+
+

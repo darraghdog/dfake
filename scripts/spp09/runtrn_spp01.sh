4f681f175b0afc4df6f6ac715998d4b3f570a643
dhanley2 <darragh_hanley@optum.com>
2020-01-05 12:41:45 +0000

    Test Densenet121

---
+++ scripts/spp09/runtrn_spp01.sh
@@ -0,0 +1,14 @@
+N_GPU=1
+WDIR='spp09'
+FOLD=0
+SIZE='224'
+ACCUM=1
+
+for LR in 0.00001
+do
+    bsub  -q low2 -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:dfake \
+            -m dbslp1827 -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/dfake/scripts/$WDIR  && python3 spp.py  \
+            --wtspath weights  --fold $FOLD  --rootpath /share/dhanley2/dfake/ --metafile trainmeta.csv.gz  \
+            --accum $ACCUM --lrgamma 0.8 --start 0 --imgpath data/mount/npimg08 --size $SIZE --batchsize 16 --lr $LR \
+            --start 0 --epochs 15"
+done

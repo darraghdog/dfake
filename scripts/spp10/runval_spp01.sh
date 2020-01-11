5907072df42bcb54b896d204cee7f0ca5b3db29a
darraghdog <darragh.hanley@gmail.com>
2020-01-11 13:17:05 -0600

    SPP synch

--- scripts/spp10/runval_spp01.sh
+++ scripts/spp10/runval_spp01.sh
@@ -1,17 +1,14 @@
 N_GPU=1
-WDIR='spp08'
+WDIR='spp10'
 FOLD=0
 SIZE='224'
 ACCUM=1
 
-# bsub  -q low2 -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:dfake \
-#             -m dbslp1829 -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/dfake/scripts/$WDIR  && python3 spp.py  \
-#             --wtspath weights  --fold $FOLD  --rootpath /share/dhanley2/dfake/ --metafile trainmeta.csv.gz  \
-#             --accum $ACCUM --lrgamma 0.8 --imgpath data/mount/npimg08 --size $SIZE --batchsize 16 --lr 0.00001 \
-#             --infer VAL --start 0 --epochs 12"
-
-bsub  -q low2 -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:dfake \
-            -m dbslp1829 -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/dfake/scripts/$WDIR  && python3 spp.py  \
+for LR in 0.00001
+do
+    bsub  -q lowpriority -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:dfake \
+            -m dbslp1827 -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/dfake/scripts/$WDIR  && python3 spp.py  \
             --wtspath weights  --fold $FOLD  --rootpath /share/dhanley2/dfake/ --metafile trainmeta.csv.gz  \
-            --accum $ACCUM --lrgamma 0.8 --imgpath data/mount/npimg10 --size $SIZE --batchsize 16 --lr 0.00001 \
-            --infer VAL --start 0 --epochs 12"
+            --accum $ACCUM --lrgamma 0.8 --start 0 --imgpath 'data/mount/npimg08'  \
+            --infer VAL --batchsize 16 --lr $LR  --size $SIZE --start 0 --epochs 8"
+done
--- scripts/spp11/runtrn_spp124.sh
+++ scripts/spp11/runtrn_spp124.sh
@@ -8,8 +8,8 @@ BSIZE=16
 for LR in 0.00001
 do
     bsub  -q lowpriority -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:dfake \
-            -m dbslp1829 -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/dfake/scripts/$WDIR  && python3 spp.py  \
+            -m dbslp1827 -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/dfake/scripts/$WDIR  && python3 spp124.py  \
             --wtspath weights  --fold $FOLD  --rootpath /share/dhanley2/dfake/ --metafile trainmeta.csv.gz  \
             --accum $ACCUM --lrgamma 0.8 --start 0 --imgpath 'data/mount/npimg11'  \
-            --batchsize $BSIZE --lr $LR  --size $SIZE --start 8 --epochs 12"
+            --batchsize $BSIZE --lr $LR  --size $SIZE --start 0 --epochs 10"
 done

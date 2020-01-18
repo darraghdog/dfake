8c44654570e0aa1c8ad3ae93e6ead96006b69c54
darraghdog <darragh.hanley@gmail.com>
2020-01-18 09:04:19 -0600

    TSM scripts synch

--- scripts/tsm01/runtrn_spp01.sh
+++ scripts/tsm01/runtrn_spp01.sh
@@ -3,15 +3,15 @@ WDIR='tsm01'
 FOLD=0
 SIZE='224'
 ACCUM=1
-BSIZE=8
+BSIZE=6
 
 for LR in 0.00001  #  0.0001 0.00001
 do
-    bsub  -q low2 -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:dfake \
-            -m dbslp1829 -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/dfake/scripts/$WDIR  && python3 spp.py  \
+    bsub  -q normal -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:dfake \
+            -m dbslp1897 -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/dfake/scripts/$WDIR  && python3 spp.py  \
             --wtspath weights  --fold $FOLD  --rootpath /share/dhanley2/dfake/ --metafile trainmeta.csv.gz  \
             --accum $ACCUM --imgpath data/mount/npimg08 --size $SIZE --batchsize $BSIZE --lr $LR \
-            --start 0 --epochs 12"
+            --nsegment 32 --start 0 --epochs 12"
 done
 
 
--- scripts/tsm02/sppbase.py
+++ scripts/tsm02/sppbase.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import math
 import glob
 import json
 import cv2
@@ -71,16 +70,12 @@ parser.add_option('-o', '--lrgamma', action="store", dest="lrgamma", help="Sched
 parser.add_option('-p', '--start', action="store", dest="start", help="Start epochs", default="0")
 parser.add_option('-q', '--infer', action="store", dest="infer", help="root directory", default="TRN")
 parser.add_option('-r', '--accum', action="store", dest="accum", help="accumulation steps", default="1")
-parser.add_option('-s', '--maxlen', action="store", dest="maxlen", help="Cap all frames sequences here", default="40")
-parser.add_option('-t', '--nsegment', action="store", dest="nsegment", help="TSM sequence length", default="8")
-
 
 options, args = parser.parse_args()
 INPATH = options.rootpath
 
 #INPATH='/Users/dhanley2/Documents/Personal/dfake'
 sys.path.append(os.path.join(INPATH, 'utils' ))
-from temporalshift import TSN
 from logs import get_logger
 from utils import dumpobj, loadobj, chunks, pilimg, SpatialDropout
 from sort import *
@@ -99,6 +94,7 @@ logger.info('Load params')
 for (k,v) in options.__dict__.items():
     logger.info('{}{}'.format(k.ljust(20), v))
 
+
 SEED = int(options.seed)
 SIZE = int(options.size)
 FOLD = int(options.fold)
@@ -109,13 +105,19 @@ WTSPATH = os.path.join(INPATH, options.wtspath)
 IMGDIR = os.path.join(INPATH, options.imgpath)
 EPOCHS = int(options.epochs)
 START = int(options.start)
-LR = float(options.lr)
-LRGAMMA = float(options.lrgamma)
-DECAY = float(options.decay)
-INFER = options.infer
-ACCUM = int(options.accum)
-MAXLEN = int(options.maxlen)
-NSEGMENT = int(options.nsegment)
+LR=float(options.lr)
+LRGAMMA=float(options.lrgamma)
+DECAY=float(options.decay)
+INFER=options.infer
+ACCUM=int(options.accum)
+
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+if n_gpu > 0:
+    torch.cuda.manual_seed_all(SEED)
+torch.backends.cudnn.deterministic = True
+
 
 # METAFILE='/Users/dhanley2/Documents/Personal/dfake/data/trainmeta.csv.gz'
 metadf = pd.read_csv(METAFILE)
@@ -127,23 +129,19 @@ class SPPSeqNet(nn.Module):
                  dense_units = 256, dropout = 0.2):
         # Only resnet is supported in this version
         super(SPPSeqNet, self).__init__()
-        self.sppnet = SPPNet(backbone=backbone, pool_size=pool_size, folder=WTSPATH)
+        self.sppnet = SPPNet(backbone=34, pool_size=pool_size, folder=WTSPATH)
         self.dense_units = dense_units
         self.lstm1 = nn.LSTM(embed_size, self.dense_units, bidirectional=True, batch_first=True)
         self.linear1 = nn.Linear(self.dense_units*2, self.dense_units*2)
         self.linear_out = nn.Linear(self.dense_units*2, 1)
         self.embedding_dropout = SpatialDropout(dropout)
     
-    def forward(self, x, nseg):
+    def forward(self, x):
         # Input is batch of image sequences
         batch_size, seqlen = x.size()[:2]
         # Flatten to make a single long list of frames
-        # x = x.view(batch_size * seqlen, *x.size()[2:])
-        x = x.view(batch_size * (seqlen/nseg), nseg, *x.size()[2:])
+        x = x.view(batch_size * seqlen, *x.size()[2:])
         # Pass each frame thru SPPNet
-        '''
-        Stuck here
-        '''
         emb = self.sppnet(x.permute(0,3,1,2))
         # Split back out to batch
         emb = emb.view(batch_size, seqlen, emb.size()[1])
@@ -300,19 +298,17 @@ class DFakeDataset(Dataset):
 def collatefn(batch):
     # Remove error reads
     batch = [b for b in batch if b is not None]
-    seqlen = torch.tensor([l['frames'].shape[0] for l in batch]).type(torch.DoubleTensor)
+    seqlen = torch.tensor([l['frames'].shape[0] for l in batch])
     ids = torch.tensor([l['idx'] for l in batch])
 
     maxlen = seqlen.max()    
-    # Get the seq len in segment intervals
-    padlen = min(32, math.ceil(seqlen.max()/NSEGMENT) * NSEGMENT)
-
+    maxlen=32
     # get shapes
     d0,d1,d2,d3 = batch[0]['frames'].shape
         
-    # Pad with zero frames and batch to one tensor
-    x_batch = [l['frames'][:padlen] if padlen-int(sl) < 0 else \
-         torch.cat((l['frames'], torch.zeros((padlen-int(sl),d1,d2,d3))), 0) 
+    # Pad with zero frames
+    x_batch = [l['frames'][:32] if l['frames'].shape[0] >= maxlen else \
+         torch.cat((l['frames'], torch.zeros((maxlen-sl,d1,d2,d3))), 0) 
          for l,sl in zip(batch, seqlen)]
     x_batch = torch.cat([x.unsqueeze(0) for x in x_batch])
     
@@ -327,40 +323,30 @@ logger.info('Create loaders...')
 # BATCHSIZE=2
 trndf = metadf.query('fold != @FOLD').reset_index(drop=True)
 valdf = metadf.query('fold == @FOLD').reset_index(drop=True)
+# trndf = trndf.head(200)
+# valdf = valdf.head(200)
 
-trndataset = DFakeDataset(trndf, IMGDIR, train = True, val = False, labels = True, maxlen = MAXLEN)
-valdataset = DFakeDataset(valdf, IMGDIR, train = False, val = True, labels = False, maxlen = MAXLEN)
-trnloader = DataLoader(trndataset, batch_size=BATCHSIZE, shuffle=True, num_workers=16, collate_fn=collatefn)
-valloader = DataLoader(valdataset, batch_size=BATCHSIZE*2, shuffle=False, num_workers=16, collate_fn=collatefn)
+trndataset = DFakeDataset(trndf, IMGDIR, train = True, val = False, labels = True, maxlen = 32)
+valdataset = DFakeDataset(valdf, IMGDIR, train = False, val = True, labels = False, maxlen = 32)
+trnloader = DataLoader(trndataset, batch_size=BATCHSIZE, shuffle=True, num_workers=8, collate_fn=collatefn)
+valloader = DataLoader(valdataset, batch_size=BATCHSIZE, shuffle=False, num_workers=8, collate_fn=collatefn)
 
 
 logger.info('Create model')
-'''
 poolsize=(1, 2, 6)
 embedsize = 512*sum(i**2 for i in poolsize)
+'''
 model = SPPSeqNet(backbone=50, pool_size=poolsize, dense_units = 256, \
                   dropout = 0.2, embed_size = embedsize)
 '''
-'''
-del TSN
-from utils.temporalshift import TSN
-folder='/Users/dhanley2/Documents/Personal/dfake/weights'
+from tsm import TSN
 NSEGMENT=32
-model = TSN(num_class=1, num_segments=NSEGMENT, modality='RGB', dropout=0.0, \
+model = TSN(num_class=1, num_segments=NSEGMENT, modality='RGB', dropout=0.5, \
             base_model='resnet50', img_feature_dim=224, pretrain='imagenet', \
             temporal_pool=False, is_shift = True, shift_div=8, shift_place='blockres', \
-            conv_head_dim = 512, pool_size = (1,2,6), \
-            partial_bn=False, dense_units=256, load_pretrain = True)
-output_model_file = '{}/tsmresnet{}.pth'.format(folder, '50')
-torch.save(model.state_dict(), output_model_file)
-'''
-model = TSN(num_class=1, num_segments=NSEGMENT, modality='RGB', dropout=0.0, \
-            base_model='resnet50', img_feature_dim=224, pretrain='imagenet', \
-            temporal_pool=False, is_shift = True, shift_div=8, shift_place='blockres', \
-            conv_head_dim = 512, pool_size = (1,2,6), \
-            partial_bn=False, dense_units=256, load_pretrain = False)
-input_model_file = os.path.join(WTSFILES, 'tsmresnet50.pth')
-model.load_state_dict(torch.load(input_model_file))
+            partial_bn=False, consensus_type='avg', custom_weights = True)
+model.load_state_dict(torch.load( os.path.join( WTSPATH, 'tsmresnet50_base.pth' ) ))
+
 model = model.to(device)
 param_optimizer = list(model.named_parameters())
 no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
@@ -374,29 +360,29 @@ scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, EPOCHS)
 
 model, optimizer = amp.initialize(model, optimizer, opt_level="O1")
 criterion = torch.nn.BCEWithLogitsLoss()
+model = torch.nn.DataParallel(model, device_ids=list(range(n_gpu)))
+
 
 ypredvalls = []
 for epoch in range(EPOCHS):
-
     LRate = scheduler.get_lr()[0]
     logger.info('Epoch {}/{} LR {:.9f}'.format(epoch, EPOCHS - 1, LRate))
     logger.info('-' * 10)
-    model_file_name = 'weights/tsm_epoch{}_lr{}_accum{}_fold{}.bin'.format(epoch, LR, ACCUM, FOLD)
-    
+    model_file_name = 'weights/sppnet_cos_epoch{}_lr{}_accum{}_fold{}.bin'.format(epoch, LR, ACCUM, FOLD)
     if epoch<START:
         model.load_state_dict(torch.load(model_file_name))
         model.to(device)
         scheduler.step()
         continue
-
     if INFER not in ['TST', 'EMB', 'VAL']:
+
         tr_loss = 0.
         for param in model.parameters():
             param.requires_grad = True
         model.train()  
-
         for step, batch in enumerate(trnloader):
             x = batch['frames'].to(device, dtype=torch.float)
+            x = x.permute(0, 1, 4, 2, 3)
             y = batch['labels'].to(device, dtype=torch.float)
             x = torch.autograd.Variable(x, requires_grad=True)
             y = torch.autograd.Variable(y)
@@ -416,12 +402,9 @@ for epoch in range(EPOCHS):
             del x, y, out, batch
         torch.save(model.state_dict(), model_file_name)
         scheduler.step()
-
     else:
-
         model.load_state_dict(torch.load(model_file_name))
         model.to(device)
-
     if INFER in ['VAL', 'TRN']:
         model.eval()
         ypredval = []
@@ -429,6 +412,7 @@ for epoch in range(EPOCHS):
         with torch.no_grad():
             for step, batch in enumerate(valloader):
                 x = batch['frames'].to(device, dtype=torch.float)
+                x = x.permute(0, 1, 4, 2, 3)
                 out = model(x)
                 out = torch.sigmoid(out)
                 ypredval.append(out.cpu().detach().numpy())

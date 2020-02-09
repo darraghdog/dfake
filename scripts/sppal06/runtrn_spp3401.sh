73c54a470163b84f6d5cd3bdb3808f61b63c5664
dhanley2 <darragh_hanley@optum.com>
2020-02-09 10:17:28 +0000

    Push up 34

---
+++ scripts/sppal06/runtrn_spp3401.sh
@@ -0,0 +1,14 @@
+N_GPU=1
+WDIR='spp14'
+FOLD=0
+SIZE='224'
+ACCUM=1
+
+for LR in 0.00001  #  0.0001 0.00001
+do
+    bsub  -q low2 -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:dfake \
+            -m dbslp1827 -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/dfake/scripts/$WDIR  && python3 spp34.py  \
+            --wtspath weights  --fold $FOLD  --rootpath /share/dhanley2/dfake/ --metafile trainmeta.csv.gz  \
+            --accum $ACCUM --imgpath data/mount/deepfake/prepared_data_v4/cropped_faces_raw/ --size $SIZE --batchsize 8 --lr $LR \
+            --start 0 --epochs 12"
+done

023919dc0667ea3a6a63e988ae0bf21221a94315
dhanley2 <darragh_hanley@optum.com>
2020-02-14 22:18:31 +0000

    Base TSM

---
+++ scripts/tsm06/runtrn_sppbase01.sh
@@ -0,0 +1,15 @@
+N_GPU=1
+WDIR='tsm06'
+FOLD=0
+SIZE='224'
+ACCUM=1
+BSIZE=3
+
+for LR in 0.00005  #  0.0001 0.00001
+do
+    bsub  -q normal -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:dfake \
+            -m dbslp1828 -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/dfake/scripts/$WDIR  && python3 spp.py  \
+            --wtspath weights  --fold $FOLD  --rootpath /share/dhanley2/dfake/ --metafile trainmeta.csv.gz  \
+            --accum $ACCUM --imgpath data/mount/npimg08 --size $SIZE --batchsize $BSIZE --lr $LR \
+            --maxlen 48 --nsegment 12 --start 0 --epochs 20 --skip 4"
+done

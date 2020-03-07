0b5ea40af728e3010794800d66a74aa260b063c2
darraghdog <darragh.hanley@gmail.com>
2020-03-07 04:27:52 -0600

    Update approaches

---
+++ scripts/spp40/runtrn_spp01_b1ns2.sh
@@ -0,0 +1,19 @@
+N_GPU=1
+WDIR='spp40'
+FOLD=0
+SIZE='224'
+ACCUM=1
+BSIZE=4
+LR=0.00005
+REAL=4
+FAKE=1
+
+for FOLD in 0 # 3 4 # 0 1 2
+do
+    bsub  -q low2 -gpu "num=$N_GPU:mode=exclusive_process" -app gpu -n =$N_GPU  -env LSB_CONTAINER_IMAGE=darraghdog/kaggle:dfake5 \
+            -m dbslp1897  -n 1 -R "span[ptile=4]" -o log_train_%J  sh -c "cd /share/dhanley2/dfake/scripts/$WDIR && python3 spp2.py \
+            --wtspath weights  --fold $FOLD  --rootpath /share/dhanley2/dfake/ --metafile trainmeta.csv.gz  \
+            --accum $ACCUM --imgpath data/mount/npimg08 --size $SIZE --batchsize $BSIZE --lr $LR \
+            --skip 4 --start 1 --epochs 20 --arch tf_efficientnet_b1_ns \
+            --origsamp $FAKE --fakesamp $FAKE --realsamp $REAL "
+done
